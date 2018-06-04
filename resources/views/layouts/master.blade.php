88e0c1f669461d7e1fe6a9f41c9d03f8711e1725
zinmyintnaung <zinmyintnaung@gmail.com>
2018-06-04 17:21:37 +0800

    layout added

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    @yield('styles')
+</head>
+<body>
+    @include('partials.header')
+    <div class="container">
+        @yield('content')
+    </div>
+    
+    
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    @yield('scripts')
+</body>
+</html>
---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+@section('title')
+    stripe-cart
+@endsection
+
+@section('content')
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://d30a6s96kk7rhm.cloudfront.net/original/readings/978/075/156/9780751565355.jpg" alt="" style="max-height:250px;" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Soluta libero, magnam veritatis, eos fuga magni officia veniam quam 
+        exercitationem molestias saepe natus incidunt nemo delectus neque pariatur tenetur. 
+        Quos, accusantium!</p>
+        <div>
+            <a href="#" class="btn btn-default" role="button">Button</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://d30a6s96kk7rhm.cloudfront.net/original/readings/978/075/156/9780751565355.jpg" alt="" style="max-height:250px;" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Soluta libero, magnam veritatis, eos fuga magni officia veniam quam 
+        exercitationem molestias saepe natus incidunt nemo delectus neque pariatur tenetur. 
+        Quos, accusantium!</p>
+        <div>
+            <a href="#" class="btn btn-default" role="button">Button</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://d30a6s96kk7rhm.cloudfront.net/original/readings/978/075/156/9780751565355.jpg" alt="" style="max-height:250px;" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Soluta libero, magnam veritatis, eos fuga magni officia veniam quam 
+        exercitationem molestias saepe natus incidunt nemo delectus neque pariatur tenetur. 
+        Quos, accusantium!</p>
+        <div>
+            <a href="#" class="btn btn-default" role="button">Button</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
